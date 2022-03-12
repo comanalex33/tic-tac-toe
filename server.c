f36b4ed2c1a0cf7d26d2af523bdacddb9ac83031
Coman Alex <comanalex33@yahoo.com>
2022-03-12 17:29:27 +0200

    Check table winner function added

--- server.c
+++ server.c
@@ -28,6 +28,31 @@ int emptyPosition(int line, int column, char board[][4]) {
     return 0;
 }
 
+int checkWinner(char board[][4]) {
+    for(int i = 0; i < 3; i++) {
+        if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ') {
+            if(board[i][0] == 'o')
+                return 1;
+            else
+                return 2;
+        }
+        if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != ' ') {
+           if(board[0][i] == 'o')
+                return 1;
+            else
+                return 2;
+        }
+    }
+    if((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ') ||
+       (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')) {
+       if(board[1][1] == 'o')
+                return 1;
+            else
+                return 2;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     int sockfd, newsockfd, newsockfd2, portno, clilen;
