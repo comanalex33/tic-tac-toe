941f06571761d1bd05aa36b9a31f0841821e6547
Sabina Cenda <sabina.cenda17@gmail.com>
2022-03-12 17:06:10 +0200

    Added board initialization and client-server connection

--- server.c
+++ server.c
@@ -7,6 +7,13 @@
 
 #include <string.h>
 
+void initBoard(char board[][4])
+{
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            board[i][j] = ' ';
+}
+
 int main(int argc, char *argv[])
 {
     int sockfd, newsockfd, newsockfd2, portno, clilen;
@@ -20,6 +27,8 @@ int main(int argc, char *argv[])
         perror("ERROR: Port not provided");
         exit(1);
     }
+
+    initBoard(board);
 
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -37,6 +46,43 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
+
+    /* Now bind the host address using bind() call.*/
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    /* Now start listening for the clients, here process will
+     * go in sleep mode and will wait for the incoming connection
+     */
+
+    listen(sockfd, 0);
+    clilen = sizeof(cli_addr);
+
+    /* Accept actual connection from the client */
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    // newsockfd2 = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+    if (newsockfd < 0)
+    {
+        perror("ERROR on accept");
+        exit(1);
+    }
+    
+    while (1)
+    {
+        int line, column;
+        if(read(newsockfd, &line, sizeof(int)) < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+        if(read(newsockfd, &column, sizeof(int)) < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+    }
 
     return 0;
 }
